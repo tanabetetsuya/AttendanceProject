336d1058ef8793330a5981a4869d0c17d080ee85
user <user@LAPTOP-VBOH87L6.elecom>
2025-08-23 22:46:28 +0900

    ログインしている状態での("/")へのGETリクエストをユーザ権限によって分岐する処理を追加

--- src/main/java/com/attendance/controller/LoginController.java
+++ src/main/java/com/attendance/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.attendance.controller; // このファイルが属するパッケー
 
 // 必要なクラスをインポートします
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +10,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller // このクラスがWebコントローラーであることを示します
 public class LoginController {
 	
+	@GetMapping("/admin/index")  // "/admin/login"というURLに対するGETリクエストを処理します
+	public String adminIndex() {
+		return "admin/index";
+	}
 	
-    @GetMapping("/user/login") // "/login"というURLに対するGETリクエストを処理します
-    public String login() {
+	
+    @GetMapping("/user/login") // "/user/login"というURLに対するGETリクエストを処理します
+    public String userLogin() {
         return "user/login";  // login.htmlを表示します
     }
     
     @GetMapping("/") // ルートURL ("/") に対するGETリクエストを処理します
     public String redirectToIndex() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication(); // 現在のユーザーの認証情報を取得します
-        if (authentication != null && authentication.isAuthenticated()) { // ユーザーがログインしている場合
-            return "redirect:/user/index";  // "/user/index"にリダイレクトします
+        if (authentication != null && authentication.isAuthenticated()
+                && !"anonymousUser".equals(authentication.getPrincipal())) { //ユーザがログインしている場合かつ権限によって表示する画面を決定
+
+            // 権限をチェック
+            for (GrantedAuthority authority : authentication.getAuthorities()) {
+                String role = authority.getAuthority();
+                if ("ROLE_ADMIN".equals(role)) {
+                    return "redirect:/admin/index";
+                } else if ("ROLE_USER".equals(role)) {
+                    return "redirect:/user/index";
+                }
+            }
         }
         return "redirect:/user/login"; // ユーザーがログインしていない場合、"/login"にリダイレクトします
     }
