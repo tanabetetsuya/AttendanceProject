d4ba9def54bf9164ecd88019f78ade9873ae5ffc
user <user@LAPTOP-VBOH87L6>
2025-08-25 17:14:29 +0900

    型をIntegerに変更

--- src/main/java/com/attendance/repository/UserRepository.java
+++ src/main/java/com/attendance/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.attendance.model.User;
 
 // UserRepositoryというインターフェースを作成します。JpaRepositoryを拡張して、UserオブジェクトとそれらのIDとしてLong型を扱えるようにします。
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     // ユーザー名でユーザーを探すメソッド。ユーザー名をパラメータとして渡すと、そのユーザー名を持つユーザーをデータベースから探して返します。
 	User findByName(String name);
 }
