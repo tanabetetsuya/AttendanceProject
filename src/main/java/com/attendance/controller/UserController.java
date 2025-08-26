72e0ab0885576244eb9c1b4101501abe918b58b9
user <user@LAPTOP-VBOH87L6>
2025-08-26 17:38:02 +0900

    誤打刻申請処理の追加

--- src/main/java/com/attendance/controller/UserController.java
+++ src/main/java/com/attendance/controller/UserController.java
@@ -10,19 +10,47 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.attendance.model.MissStampingApply;
 import com.attendance.model.TimeRecord;
 import com.attendance.model.User;
+import com.attendance.repository.MissStampingApplyRepository;
+import com.attendance.repository.UserRepository;
 import com.attendance.service.UserService;
 
 @Controller
 public class UserController {
 
     private final UserService service;
+    private final UserRepository userRepository;
+    private final MissStampingApplyRepository missStampingApplyRepository;
 
-    public UserController(UserService service) {
+    public UserController(UserService service, UserRepository userRepository, MissStampingApplyRepository missStampingApplyRepository) {
         this.service = service;
+        this.userRepository = userRepository;
+        this.missStampingApplyRepository = missStampingApplyRepository;
     }
-
+    
+    
+    @GetMapping("/user/StampingCorrectionForm/{recordId}/{userId}")
+    public String showStampingCorrectionForm(@PathVariable int recordId, @PathVariable int userId, Model model) {
+    	TimeRecord record = service.getTimeRecordById(recordId);
+    	User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+    	model.addAttribute("user", user);
+    	model.addAttribute("record", record);
+    	model.addAttribute("userId", userId);
+    	return "user/StampingCorrectForm";
+    }
+    
+    @PostMapping("/user/StampingCorrectionForm/{recordId}/{userId}")
+    public String saveStampingCorrectionForm(@PathVariable int recordId, @PathVariable int userId, MissStampingApply miss) {
+    	TimeRecord record = service.getTimeRecordById(recordId);
+    	User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+    	miss.setTimerecord(record);
+    	miss.setUser(user);
+    	missStampingApplyRepository.save(miss);
+    	return "redirect:/user/AttendanceHandling";
+    }
+    
     @GetMapping("/user/{id}")
     public String viewTimeRecords(@PathVariable int id, Model model) {
         model.addAttribute("user", service.getUserById(id));
@@ -40,12 +68,11 @@ public class UserController {
     
     @GetMapping("/user/AttendanceHandling")
     public String showMyPage(Model model, Principal principal) {
-        // ログインユーザの名前を取得
+		// ログインユーザの名前を取得
     	String username = principal.getName();
 
         // DBからユーザ情報を取得
         User user = service.findByUsername(username);
-        
         // modelに渡す
         model.addAttribute("user", user);
         // 勤怠記録もmodelに渡す
@@ -54,7 +81,7 @@ public class UserController {
         
         // 登録フォーム用
         model.addAttribute("recordForm", new TimeRecord());
-
+            
         
         return "user/AttendanceHandling";
     }
