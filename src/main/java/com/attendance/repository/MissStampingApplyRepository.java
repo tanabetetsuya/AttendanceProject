6c3f5f2ef462d8b12bd1358b1509e9e4fc891be8
user <user@LAPTOP-VBOH87L6>
2025-08-27 14:40:15 +0900

    MissStampingApplyRepositoryの作成

--- src/main/java/com/attendance/repository/MissStampingApplyRepository.java
+++ src/main/java/com/attendance/repository/MissStampingApplyRepository.java
@@ -1,5 +1,6 @@
 package com.attendance.repository;
 
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +9,10 @@ import com.attendance.model.MissStampingApply;
 
 // MissStampingApplyRepositoryというインターフェースを作成します。JpaRepositoryを拡張して、MissStampingApplyオブジェクトとそれらのIDをInteger型で扱えるようにします。
 public interface MissStampingApplyRepository extends JpaRepository<MissStampingApply, Integer>{
-	
+	// ID昇順で取得
+    List<MissStampingApply> findAllByOrderByMissstampingIdAsc();
+    
+    // 任意：特定ユーザの申請だけ取得したい場合
+    List<MissStampingApply> findByUserIdOrderByMissstampingIdAsc(int userId);
+
 }
